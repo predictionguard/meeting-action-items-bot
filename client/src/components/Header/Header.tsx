ab678c275b2f03b020680dd42ab4151e9fdda1f1
aramasethu <aishwarya.ramasethu@gmail.com>
2024-12-04 11:48:01 -0500

    header change for PG

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,10 +3,9 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="Header">
-      <h1 className="Header--heading">Get action items from your meeting</h1>
+      <h1 className="Header--heading">Get updates from standup</h1>
       <p className="Header--subheading">
-        Enter your current meeting link and get a list of action items at the
-        end of your meeting.
+        Enter your current meeting link and get a list of updates after the standup.
       </p>
     </div>
   );
