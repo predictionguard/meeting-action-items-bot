6a0a483d516e378f3b70166c14bfa66be823a3de
aramasethu <aishwarya.ramasethu@gmail.com>
2024-12-06 16:02:24 -0500

    modified index.js for the query route

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
+// index.js
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const apiRoutes = require("./routes/api");
-//const webhookRoutes1 = require("./routes/webhook");
 const webhookRoutes = require("./routes/webhook");
+const queryRoutes = require("./routes/queryRoutes"); 
 
 const app = express();
 
@@ -13,10 +14,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the server!");
-  });
+  res.send("Welcome to the server!");
+});
+
+// Existing routes
 app.use("/api", apiRoutes);
-//app.use("/webhook", webhookRoutes1);
 app.use("/webhook", webhookRoutes);
+app.use("/queryRoutes", queryRoutes); 
 
 app.listen(3000, () => console.log("Listening on port 3000."));
