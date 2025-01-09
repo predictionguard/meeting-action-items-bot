5bf90ee1378040cb0733ae0c4898e1825b83a036
aramasethu <aishwarya.ramasethu@gmail.com>
2025-01-09 14:40:43 -0500

    Cleaned script

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { executePythonScript } = require("./rag_load");
 const axios = require("axios");
 const config = require("../config");
 const {
@@ -8,44 +9,46 @@ const {
   } = require("../utils");
 const { sendEvent } = require("../events");
 const router = express.Router();
-
+const fs = require('fs').promises;
 router.post("/status_change", async (req, res) => {
   const { data } = req.body;
-
+  console.log("Here is the meeting date", data.status.created_at);
   res.status(200).send("OK");
 
   if (data.status.code === "done") {
     try {
-      const meetingDateTime = data.join_at;
-      const formattedDateTime = new Date(meetingDateTime).toLocaleString(); // Human-readable format
-      const transcribeResponse = await axios.post(
-        `https://${config.recallRegion}.recall.ai/api/v1/bot/${data.bot_id}/transcribe/`,
-        {
-          headers: {
-            Authorization: `Token ${config.recallApiKey}`,
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        }
-      );
+
+      const meetingDateTime = data.status.created_at;
+      const formattedDateTime = new Date(meetingDateTime).toLocaleString(); 
+      // const transcriptFilePath = `transcripts/32faabb5-bcc0-4fde-ac36-aba309355ca4_transcript.txt`;
+      // const transcriptContent = await fs.readFile(transcriptFilePath, "utf-8");
+      // let transcript;
+      // try {
+      //   transcript = JSON.parse(transcriptContent);
+      //   if (!Array.isArray(transcript)) {
+      //     throw new Error("Parsed transcript is not an array");
+      //   }
+      // } catch (error) {
+      //   console.error("Error parsing transcript file:", error.message);
+      //   throw new Error("Invalid transcript format");
+      // }
+      // if (transcript.length === 0) {
+      //   sendEvent({ error: "No transcript found" });
+      //   return;
+      // }
+      // console.log("transcript processed");
+
       const transcriptResponse = await axios.get(
-        `https://${config.recallRegion}.recall.ai/api/v1/bot/${data.bot_id}/transcript`,
-        {
-          headers: {
-            Authorization: `Token ${config.recallApiKey}`,
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-
-      const transcript = transcriptResponse.data; 
-      // error handling for empty transcript
-      if (transcript.length === 0) {
-        sendEvent({ error: "No transcript found" });
-        return;
-      }
+              `https://${config.recallRegion}.recall.ai/api/v1/bot/${data.bot_id}/transcript`,
+              {
+                headers: {
+                  Authorization: `Token ${config.recallApiKey}`,
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+      const transcript = transcriptResponse.data;
       const preprocessedTranscript = preprocessGoogleMeetTranscript(transcript);
       console.log("transcript precleaned");
       const cleanTranscript = await cleanUpTranscript(preprocessedTranscript);
@@ -53,31 +56,13 @@ router.post("/status_change", async (req, res) => {
       const actionItems = await extractActionItems(cleanTranscript);
       console.log("transcript action items", actionItems);
       sendEvent(actionItems);
-      const inputData = JSON.stringify({
+      const inputData = {
         meeting_id: data.bot_id, 
-        transcript: actionItems,
+        transcript: cleanTranscript,
         meetingDateTime: formattedDateTime,
-      });
+      };
 
-      // Execute Python script
-      const pythonScript = "RAG_pipeline.py";
-      const pythonProcess = spawn("python3", [pythonScript]);
-
-      pythonProcess.stdin.write(inputData);
-      pythonProcess.stdin.end();
-
-      // Handle Python script output
-      pythonProcess.stdout.on("data", (data) => {
-        console.log(`Python (stdout): ${data}`);
-      });
-
-      pythonProcess.stderr.on("data", (data) => {
-        console.error(`Python (stderr): ${data}`);
-      });
-
-      pythonProcess.on("close", (code) => {
-        console.log(`Python process exited with code ${code}`);
-      });
+      await executePythonScript(inputData);
     } catch (error) {
       console.error(error);
       sendEvent({ error: "Error extracting action items" });
